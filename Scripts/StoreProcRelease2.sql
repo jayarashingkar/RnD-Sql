30a17cf3be0464cda92dcef825ea94b211e2ca0f
Jaya <jaya.rashingkar@universalalloy.com>
2018-03-15 12:40:19 -0400

    added store procs : 1. [RNDLocation_Update] 2. RNDLocation_Insert 3. StudyType_Insert 4. RNDStudyType_Update

---
+++ Scripts/StoreProcRelease2.sql
@@ -0,0 +1,110 @@
+﻿/****** Object:  StoredProcedure [dbo].[RNDStudyType_Update]     ******/
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE
+ [dbo].RNDStudyType_Update
+	@RecID [int],
+   @TypeDesc [varchar](30)          
+AS
+BEGIN
+	
+	UPDATE [dbo].[RNDStudyType]
+	   SET [TypeDesc] = @TypeDesc 
+	 WHERE RecID = @RecID   
+END
+GO
+
+/****** Object:  StoredProcedure [dbo].[StudyType_Insert]     ******/
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE
+ [dbo].StudyType_Insert
+   @TypeDesc [varchar](30)          
+AS
+BEGIN	
+	DECLARE @TypeStudy char(2)
+	
+	SET @TypeStudy = cast((SELECT MAX(RecID) FROM [RNDStudyType]) AS char(2) )
+
+	INSERT INTO [dbo].[RNDStudyType]
+           ([TypeStudy]
+           ,[TypeDesc])
+     VALUES
+           (@TypeStudy
+           ,@TypeDesc)
+
+	 DECLARE @RecId int
+	 SELECT @RecId = [RecId]
+    FROM [dbo].[RNDStudyType]
+    WHERE @@ROWCOUNT > 0 AND [RecId] = scope_identity()
+END
+GO
+
+
+/****** Object:  StoredProcedure [dbo].[[RNDLocation]]     ******/
+
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE
+ [dbo].RNDLocation_Insert
+    @PlantDesc char(20),
+    @PlantState char(2),
+    @PlantType tinyint           
+AS
+BEGIN	
+	DECLARE @Plant smallint
+	
+	SET @Plant = (SELECT MAX(RecID) FROM [RNDLocation]) 
+
+	INSERT INTO [dbo].[RNDLocation]
+           ([Plant]
+           ,[PlantDesc]
+           ,[PlantState]
+           ,[PlantType])
+     VALUES
+           (@Plant, 
+           @PlantDesc, 
+           @PlantState, 
+           @PlantType)
+
+	 DECLARE @RecId int
+	 SELECT @RecId = [RecId]
+    FROM [dbo].[RNDLocation]
+    WHERE @@ROWCOUNT > 0 AND [RecId] = scope_identity()
+END
+GO
+
+USE [RDB]
+GO
+/****** Object:  StoredProcedure [dbo].[RNDLocation_Update]    Script Date: 3/15/2018 12:00:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE
+ [dbo].[RNDLocation_Update]
+	@RecID [int],  
+    @PlantDesc char(20),
+    @PlantState char(2),
+    @PlantType tinyint        
+AS
+BEGIN	
+	UPDATE [dbo].[RNDLocation]
+	SET 
+		[PlantDesc] = @PlantDesc
+      ,[PlantState] =  @PlantState 
+      ,[PlantType] =  @PlantType
+	 WHERE RecID = @RecID   
+END
