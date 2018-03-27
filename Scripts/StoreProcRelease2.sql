ebeaae025cf81740cfd057107f12545adb0db8cd
Jaya <jaya.rashingkar@universalalloy.com>
2018-03-27 12:34:42 -0400

    updated store procs

--- Scripts/StoreProcRelease2.sql
+++ Scripts/StoreProcRelease2.sql
@@ -20,7 +20,8 @@
 
 /****** Object:  StoredProcedure [dbo].[StudyType_Insert]     ******/
 
-
+USE [RDB]
+GO
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -58,7 +59,8 @@ GO
 
 
 /****** Object:  StoredProcedure [dbo].[[RNDLocation]]     ******/
-
+USE [RDB]
+GO
 
 
 SET ANSI_NULLS ON
@@ -99,8 +101,7 @@ END
 
 GO
 
-USE [RDB]
-GO
+
 /****** Object:  StoredProcedure [dbo].[RNDLocation_Update]    Script Date: 3/15/2018 12:00:16 PM ******/
 --SET ANSI_NULLS ON
 --GO
@@ -121,3 +122,229 @@ GO
 --      ,[PlantType] =  @PlantType
 --	 WHERE RecID = @RecID   
 --END
+
+
+/****** Object:  StoredProcedure [dbo].[RNDImportTestList_READ]     ******/
+
+USE [RDB]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+           
+CREATE PROCEDURE 
+[dbo].RNDImportTestList_READ 
+@Active char 
+AS
+	BEGIN
+		SELECT [RecID]
+      ,[TestDesc]
+      ,[TestTableName]
+      ,[Active]
+      ,[TabPos]
+  FROM [dbo].[RNDTestList]
+  WHERE Active = @Active
+	END
+GO
+
+USE [RDB]
+GO
+/****** Object:  StoredProcedure [dbo].[RNDLocation_Insert]    Script Date: 3/22/2018 12:36:33 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE
+ [dbo].RNDSCCResults_Insert
+ (          @SelectedTests varchar(max),
+		   @StressKsi char(5) = null,
+           @TimeDays char(2) = null,
+           @TestStatus char(5) = null,
+           @SpeciComment char(50) = null,
+           @Operator char(20) = null,
+           @TestStartDate datetime = null,
+           @TestEndDate datetime = null,
+           @EntryBy char(25),
+           @EntryDate datetime
+)       
+AS
+BEGIN	
+		DECLARE @WorkStudyID char(10)
+        DECLARE @MillLotNo int
+        DECLARE @LotID char(10)
+        DECLARE @TestingNo int
+		DECLARE @maxRecID INT	
+
+		IF (@SelectedTests IS NOT NULL)
+		BEGIN
+			DECLARE @x XML 
+			SELECT 	@x = CAST('<A>'+ REPLACE(@SelectedTests,',','</A><A>')+ '</A>' AS XML);		
+			SELECT TestingNo, [WorkStudyID],[LotID],[MillLotNo], StressKsi = @StressKsi,
+			   TimeDays =@TimeDays,
+			   TestStatus =@TestStatus,
+			   SpeciComment =@SpeciComment,
+			   Operator =@Operator,
+			   TestStartDate =@TestStartDate,
+			   TestEndDate =@TestEndDate,
+			   EntryBy =@EntryBy,
+			   EntryDate =@EntryDate,
+			   [Completed] = '1'
+
+			INTO #tempTestingNo			
+			FROM RNDTesting
+			WHERE TestingNo IN (SELECT t.value('.', 'int') AS inVal FROM @x.nodes('/A') AS x(t))
+
+			SET @maxRecID = (SELECT MAX(RecId) FROM  [RNDSCCResults])
+
+			INSERT INTO [dbo].[RNDSCCResults]
+           ([WorkStudyID]
+           ,[MillLotNo]
+           ,[LotID]
+           ,[TestingNo]
+           ,[StressKsi]
+           ,[TimeDays]
+           ,[TestStatus]
+           ,[SpeciComment]
+           ,[Operator]
+           ,[TestStartDate]
+           ,[TestEndDate]
+           ,[EntryBy]
+           ,[EntryDate]
+           ,[Completed])
+			 SELECT 
+            [WorkStudyID]
+           ,[MillLotNo]
+           ,[LotID]
+           ,[TestingNo]
+           ,[StressKsi]
+           ,[TimeDays]
+           ,[TestStatus]
+           ,[SpeciComment]
+           ,[Operator]
+           ,[TestStartDate]
+           ,[TestEndDate]
+           ,[EntryBy]
+           ,[EntryDate]
+           ,[Completed]
+		   FROM #tempTestingNo
+
+		select TestingNo 
+		FROM [dbo].[RNDSCCResults]
+		where RecID > @maxRecID
+		
+		DROP TABLE #tempTestingNo
+	END
+END
+
+
+USE [RDB]
+GO
+/****** Object:  StoredProcedure [dbo].[RNDLocation_Insert]    Script Date: 3/22/2018 12:36:33 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE
+ [dbo].RNDEXCOResults_Insert
+ (         @SelectedTests varchar(max),
+		   @ExcoRating char(4),
+           @StartWT char(8),
+           @FinalWT char(8),
+           @ExposedArea char(8),
+           @StartpH char(5),
+           @FinalpH char(5),
+           @SpeciComment char(50),
+           @Operator char(20),
+           @TestDate datetime,
+           @TimeHrs char(2),
+           @TimeMns char(2),
+           @BatchNo char(4),
+           @EntryBy char(25),
+           @EntryDate datetime
+)       
+AS
+BEGIN	
+		DECLARE @WorkStudyID char(10)
+        DECLARE @MillLotNo int
+        DECLARE @LotID char(10)
+        DECLARE @TestingNo int
+		DECLARE @maxRecID INT	
+
+		IF (@SelectedTests IS NOT NULL)
+		BEGIN
+			DECLARE @x XML 
+			SELECT 	@x = CAST('<A>'+ REPLACE(@SelectedTests,',','</A><A>')+ '</A>' AS XML);		
+			SELECT TestingNo, [WorkStudyID],[LotID],[MillLotNo], 
+			ExcoRating =@ExcoRating,
+          StartWT =@StartWT,
+          FinalWT =@FinalWT,
+          ExposedArea =@ExposedArea,
+          StartpH =@StartpH,
+          FinalpH =@FinalpH,
+          SpeciComment =@SpeciComment,
+          Operator =@Operator,
+          TestDate =@TestDate,
+          TimeHrs =@TimeHrs,
+          TimeMns =@TimeMns,
+          BatchNo =@BatchNo,
+			   EntryBy =@EntryBy,
+			   EntryDate =@EntryDate,
+			   [Completed] = '1'
+
+			INTO #tempTestingNo			
+			FROM RNDTesting
+			WHERE TestingNo IN (SELECT t.value('.', 'int') AS inVal FROM @x.nodes('/A') AS x(t))
+
+			SET @maxRecID = (SELECT MAX(RecId) FROM  [RNDExcoResults])
+
+			INSERT INTO [dbo].[RNDExcoResults]
+          ([WorkStudyID]
+           ,[MillLotNo]
+           ,[LotID]
+           ,[TestingNo]
+           ,[ExcoRating]
+           ,[StartWT]
+           ,[FinalWT]
+           ,[ExposedArea]
+           ,[StartpH]
+           ,[FinalpH]
+           ,[SpeciComment]
+           ,[Operator]
+           ,[TestDate]
+           ,[TimeHrs]
+           ,[TimeMns]
+           ,[BatchNo]
+           ,[EntryBy]
+           ,[EntryDate]
+           ,[Completed])
+			 SELECT 
+           @WorkStudyID
+           ,@MillLotNo
+           ,@LotID
+           ,@TestingNo
+           ,@ExcoRating
+           ,@StartWT
+           ,@FinalWT
+           ,@ExposedArea
+           ,@StartpH
+           ,@FinalpH
+           ,@SpeciComment
+           ,@Operator
+           ,@TestDate
+           ,@TimeHrs
+           ,@TimeMns
+           ,@BatchNo
+           ,@EntryBy
+           ,@EntryDate
+           ,'1'
+		   FROM #tempTestingNo
+
+		select TestingNo 
+		FROM [dbo].[RNDExcoResults]
+		where RecID > @maxRecID
+		
+		DROP TABLE #tempTestingNo
+	END
+END
---
+++ UpdateTestListTable.sql
@@ -0,0 +1,33 @@
+﻿
+update RNDTestList 
+set Active = '2' where RecID =1
+update RNDTestList 
+set Active = '2' where RecID =2 
+update RNDTestList 
+set Active = '2' where RecID =3
+update RNDTestList 
+set Active = '2' where RecID =4
+update RNDTestList 
+set Active = '2' where RecID =5
+update RNDTestList 
+set Active = '2' where RecID =6
+update RNDTestList 
+set Active = '2' where RecID =7
+update RNDTestList 
+set Active = '2' where RecID =8
+update RNDTestList 
+set Active = '2' where RecID =9
+update RNDTestList 
+set Active = '3' where RecID =11
+--manual entry
+update RNDTestList 
+set Active = '3' where RecID =10
+update RNDTestList 
+set Active = '3' where RecID =14
+update RNDTestList 
+set Active = '3' where RecID =15
+update RNDTestList 
+set Active = '3' where RecID =16
+
+--unknown
+--R-cure is it manual or import ? 
